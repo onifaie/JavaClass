d3f35865b067fcbdf31c90b60c3cc39bde6c0775
onifaie <onifaie@gmail.com>
2024-09-18 00:18:23 +0300

    frist-upload

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Section1 s1 = new Section1();
+
+
+        System.out.println("Hello world!");
+        System.out.println(s1.sum(3,5));
+        System.out.println(s1.Minc(40,22));
+
+        
+    }
+
+
+
+     
+}
---
+++ src/Section1.java
@@ -0,0 +1,15 @@
+public class Section1 {
+
+
+
+    public  int sum(int x, int y){
+
+        return  x + y ;
+
+    }
+
+    public int Minc(int x, int y ){
+        return x-y;
+
+    }
+}
