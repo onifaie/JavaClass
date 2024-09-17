51f8e866a554a7bf913f20e22f5bb80be6945f86
onifaie <onifaie@gmail.com>
2024-09-18 01:09:50 +0300

    Add-Section5-3

---
+++ src/Section3.java
@@ -0,0 +1,12 @@
+public class Section3 {
+
+    public boolean TrueFalse(int x ){
+
+        if ( x > 0 )
+                return true;
+        else
+            return false;
+
+    }
+    // this my comminemt
+}
