639324d8b673c8989c75b7e34608241768e8e102
onifaie <onifaie@gmail.com>
2024-09-18 21:12:25 +0300

    Add-IFStatment

---
+++ src/Fstatment/UseStatment.java
@@ -0,0 +1,15 @@
+package Fstatment;
+
+public class UseStatment {
+
+    int i= 0;
+
+     public  int pp(){
+
+         for (int i = 0; i < 44; i++) {
+             System.out.println(i);
+         }
+         return i;
+
+     }
+}
