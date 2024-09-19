7918602ca676ac193bea8bfe9089dc8b47cf4928
onifaie <onifaie@gmail.com>
2024-09-19 19:53:54 +0300

    add-GannLevels

--- src/GannLevels/Gannlevels.java
+++ src/GannLevels/Gannlevels.java
@@ -6,21 +6,21 @@ public class Gannlevels {
     private static double[] calculateGannLevels(double high, double low) {
         double range = high - low;
         return new double[]{
-                high,
-                low + range * 0.875,
-                low + range * 0.75,
-                low + range * 0.625,
-                low + range * 0.50,  // Midpoint
-                low + range * 0.375,
-                low + range * 0.25,
-                low + range * 0.125,
-                low
+//                high,
+                Math.round(low + range * 0.875),
+                Math.round(low + range * 0.75),
+                Math.round(low + range * 0.625),
+                Math.round(low + range * 0.50),  // Midpoint
+                Math.round(low + range * 0.375),
+                Math.round(low + range * 0.25),
+                Math.round(low + range * 0.125),
+//                low
         };
     }
     private static boolean printGannLevels(double[] gannLevels) {
         System.out.println("Gann levels:");
         for (double level : gannLevels) {
-            System.out.println(level);
+            System.out.println("level is " + "   " +  level);
         }
         return false;
     }
